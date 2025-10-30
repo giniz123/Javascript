655ec68d0b115ec6398f1369e8b77a6cf0773cd5
giniz123 <giniz.osti@gmail.com>
2025-10-30 21:49:39 +1100

    js syntax, embedded & external js, variable

---
+++ script.js
@@ -0,0 +1,48 @@
+/*console.log("Hello World");
+alert("Hi there, click ok to continue");*/
+
+// ========== variable (let, var, const) (can only be char, num, $ _)
+
+// one way to define variable
+
+var name = "Giniz Osti";
+
+// another way
+// var name; // Creating or defining varialbe
+// name = "Giniz Osti"; // assigning value of variable
+
+// let add = "Melbourne";
+
+// console.log(name, add);
+
+// name = "John";
+
+// const age = 20;
+
+// age = 15; // cannot override the age data, it is const variable
+
+// console.log(age);
+
+// let firstName= "Giniz";  most used one (Camelcase)
+// let FirstName= "Giniz";
+// let FIRSTNAME= "Giniz";
+// let firstname= "Giniz";
+
+// ===== Arthimetic Operator
+const a = 5;
+const b = 6;
+const c = b;
+
+// + Addition
+// const result = a + b + c;
+
+// - Subtraction
+// const result = a - b;
+
+// ** Multiplication
+// const result = a * b;
+
+//  exponentiation
+const result = a ** b;
+
+console.log(result);
